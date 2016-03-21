516dd56f2ed005b687ad26512f7d845946d699a0
suxg003 <1410359394@qq.com>
2016-03-21 16:57:27 +0800

    增加微信link消息类型

---
+++ Weixin/src/com/imooc/po/LinkMessage.java
@@ -0,0 +1,41 @@
+package com.imooc.po;
+
+public class LinkMessage extends BaseMessage {
+	private String title;
+	private String description;
+	private String url;
+	private String msgId;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getMsgId() {
+		return msgId;
+	}
+
+	public void setMsgId(String msgId) {
+		this.msgId = msgId;
+	}
+
+}
